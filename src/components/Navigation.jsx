6ee0eb8536bca98b7c4a9245634689a1a7140881
daffaabp <daffa.budi2003@gmail.com>
2024-04-07 03:40:07 +0700

    setup react router

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+        <ul>
+            <li>
+                <NavLink to="/" reloadDocument>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/cars">Cars</NavLink>
+            </li>
+            <li>
+                <NavLink to="/contact" replace>
+                    Contact
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/about">About</NavLink>
+            </li>
+        </ul>
+    </>
+  );
+};
 
-export default Navigation
+export default Navigation;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, MemoryRouter, HashRouter } from "react-router-dom";
+import { StaticRouter } from "react-router-dom/server";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
