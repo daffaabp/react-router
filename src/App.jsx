3658320d01eea028041128d1045e6eb19cacd683
daffaabp <daffa.budi2003@gmail.com>
2024-04-07 11:00:41 +0700

    nested_Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Cars from "./pages/Cars";
-import CarDetail from './pages/CarDetail'
-// import SecondCar from './pages/SecondCar'
+// import Cars from "./pages/Cars";
+// import CarDetail from "./pages/CarDetail";
+// import SecondCar from "./pages/SecondCar";
 import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
+// import CarLayout from "./CarLayout";
+import CarsRoute from "./CarsRoute";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cars" element={<Cars />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cars/:id" element={<CarDetail />} />
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/cars/*" element={<CarsRoute />}/>
+          {/* split kode --> jangan lupa menambahkan tanda bintang */}
+			<Route path="/*" element={<NotFound />} />
       </Routes>
+
+		<Routes>
+			<Route path="/about" element={<div>sidebar</div>} />
+		</Routes>
     </>
   );
 }
--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {useOutletContext} from 'react-router-dom'
 
 const Cars = () => {
+  const {text} = useOutletContext()
   return (
-    <div>
-      Cars
-    </div>
-  )
+    <>
+      <div>Cars</div>
+      <div>{text}</div>
+    </>
+  );
 }
 
 export default Cars
