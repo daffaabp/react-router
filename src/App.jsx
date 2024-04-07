2af65d4acacb4f37868e9660f0cee227ab22e70f
daffaabp <daffa.budi2003@gmail.com>
2024-04-07 11:13:30 +0700

    useRoutes Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,65 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Cars from "./pages/Cars";
-// import CarDetail from "./pages/CarDetail";
-// import SecondCar from "./pages/SecondCar";
+import Cars from "./pages/Cars";
+import CarDetail from "./pages/CarDetail";
+import SecondCar from "./pages/SecondCar";
 import Contact from "./pages/Contact";
-import { Routes, Route } from "react-router-dom";
-// import CarLayout from "./CarLayout";
+import { Routes, Route, useRoutes } from "react-router-dom";
+import CarLayout from "./CarLayout";
 import CarsRoute from "./CarsRoute";
 import NotFound from "./pages/NotFound";
 
 function App() {
+	let element = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+    {
+      path: "/cars",
+		element: <CarLayout />,
+      children: [
+        {
+          index: true,
+          element: <Cars />,
+        },
+        {
+          path: ':id',
+          element: <CarDetail />,
+        },
+        {
+          path: 'second',
+          element: <SecondCar />,
+        },
+      ],
+    },
+  ]);
+
+
   return (
     <>
       <Navigation />
-      <Routes>
+		{element}
+      {/* <Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/about" element={<About />} />
 			<Route path="/contact" element={<Contact />} />
 			<Route path="/cars/*" element={<CarsRoute />}/>
-          {/* split kode --> jangan lupa menambahkan tanda bintang */}
 			<Route path="/*" element={<NotFound />} />
       </Routes>
 
 		<Routes>
 			<Route path="/about" element={<div>sidebar</div>} />
-		</Routes>
+		</Routes> */}
     </>
   );
 }
